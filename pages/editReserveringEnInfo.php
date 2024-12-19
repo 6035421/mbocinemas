3ed93c994422347354c99585323e958fc3ab60b4
luke hoogenboom <6035070@mborijnland.nl>
2024-12-19 10:35:48 +0100

    backup

--- pages/editReserveringEnInfo.php
+++ pages/editReserveringEnInfo.php
@@ -19,80 +19,111 @@
 
     <main class="scroll">
         <section class="column">
+            <form class="column">
 
-            <hr titleText="Data">
-            <article>
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">Datum: </div>
-                    02-002-0020
-                </div>
+                <article>
+                    <div class="rounded row contact">
+                        <div class="rounded contactHighlight">Film: </div>
+                        <input type="text" name="filmName" value="Five Nights At Freddy's">
+                    </div>
+                </article>
 
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">Tijd: </div>
-                    XO:XO
-                </div>
+                <hr titleText="Data">
+                <article>
+                    <div class="rounded row contact">
+                        <div class="rounded contactHighlight">Datum: </div>
+                        <input type="date" name="filmDatum" value="XO-XO-XO">
+                    </div>
 
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">Duur: </div>
-                    3 Uur
-                </div>
-            </article>
+                    <div class="rounded row contact">
+                        <div class="rounded contactHighlight">Tijd: </div>
+                        <input type="time" name="filmBeginTijd" value="XO-XO-XO">
+                    </div>
 
-            <hr titleText="Locatie">
-            <article>
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">Bioscoop: </div>
-                    Absolute Cinema
-                </div>
+                    <div class="rounded row contact">
+                        <div class="rounded contactHighlight">Duur: </div>
+                        <input type="time" name="filmDuur" value="XO-XO-XO">
+                    </div>
+                </article>
 
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">Zaal: </div>
-                    002
-                </div>
+                <?php
+                if ($_GET['type'] != 'add') {
+                    echo '
+    <hr titleText="Locatie">
+    <article>
+        <div class="rounded row contact">
+            <div class="rounded contactHighlight">Bioscoop: </div>
+            Absolute Cinema
+        </div>
 
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">RIj: </div>
-                    3
-                </div>
+        <div class="rounded row contact">
+            <div class="rounded contactHighlight">Zaal: </div>
+            002
+        </div>
 
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">Stoelen: </div>
-                    6 en 9
-                </div>
-            </article>
+        <div class="rounded row contact">
+            <div class="rounded contactHighlight">RIj: </div>
+            3
+        </div>
 
-            <hr titleText="Status">
+        <div class="rounded row contact">
+            <div class="rounded contactHighlight">Stoelen: </div>
+            6 en 9
+        </div>
+    </article>
 
-            <article>
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">Status: </div>
-                    Betaald
-                </div>
-            </article>
+    <hr titleText="Status">
 
-            <hr titleText="Contact">
-            <article>
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">Telefoon: </div>
-                    XO-XOXO-XOXO
-                </div>
+    <article>
+        <div class="rounded row contact">
+            <div class="rounded contactHighlight">Status: </div>
+            Betaald
+        </div>
+    </article>
 
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">E-mail: </div>
-                    XOXO@XOXO.com
-                </div>
+    <hr titleText="Contact">
+    <article>
+        <div class="rounded row contact">
+            <div class="rounded contactHighlight">Telefoon: </div>
+            XO-XOXO-XOXO
+        </div>
 
-                <div class="rounded row contact">
-                    <div class="rounded contactHighlight">Adress: </div>
-                    3336 QD, Sassenheim
-                </div>
-            </article>
+        <div class="rounded row contact">
+            <div class="rounded contactHighlight">E-mail: </div>
+            XOXO@XOXO.com
+        </div>
 
-            <button id="safe">Opslaan</button>
+        <div class="rounded row contact">
+            <div class="rounded contactHighlight">Adress: </div>
+            3336 QD, Sassenheim
+        </div>
+    </article>';
+                } else {
+                    echo '
+    <hr titleText="displayData">
+    <article>
+        <div class="rounded row contact">
+            <div class="rounded contactHighlight">Icoon: </div>
+            <input type="file" name="filmIcoon" value="XO-XO-XO">
+        </div>
+        
+        <div class="rounded row contact">
+            <div class="rounded contactHighlight">Categorie: </div>
+            <input type="text" name="filmCategorie" value="Fiction">
+        </div>
+    </article>';
+                }
+                ?>
+
+
+                <button type='submit' id="safe">Opslaan</button>
+            </form>
         </section>
     </main>
 
 
+    <script src="../js/formValidatie.js"></script>
+    <script src="../js/formStoring.js"></script>
     <script src="../mbocinemas/js/index.js"></script>
 </body>
 
